edf9942b8787ee97ea9ef66b11810f9519f47d8b
evansochieng <evansochieng121@gmail.com>
2023-11-23 08:41:24 +0300

    complete lab on sql querying

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+-- INSERT VALUES
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 34, 'M', 'chocolate', 'phlegmatic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 24, 'M', 'dark', 'melancholic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 76, 'F', 'white', 'sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 17, 'M', 'brown', 'phlegmatic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 14, 'F', 'brown', 'melancholic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 8, 'M', 'white', 'sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 23, 'F', 'chocolate', 'phlegmatic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 45, 'F', 'dark', 'choleric', 0);
